76d21b86b8be7177fe834ad0fed622a5b26dff3b
Nirvan004 <Nirvanandb2004@gmail.com>
2025-11-18 10:46:28 -0500

    added fetch product reviews function

--- apiSimulator.ts
+++ apiSimulator.ts
@@ -3,12 +3,42 @@ export const fetchProductCatalog = (): Promise<{ id: number; name: string; price
     setTimeout(() => {
         if (Math.random() < 0.8) {
         resolve([
-            { id: 1, name: "Laptop", price: 1200 },
-            { id: 2, name: "Headphones", price: 200 },
+            { id: 1, name: "Xbox Elite Controller", price: 120 },
+            { id: 2, name: "Sony WH-1000XM4", price: 300 },
         ]);
         } else {
         reject("Failed to fetch product catalog");
         }
     }, 1000);
+    });
+};
+
+export const fetchProductReviews = (
+    productId: number
+): Promise<{ reviewer: string; rating: number; comment: string }[]> => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() < 0.8) {
+                resolve([
+                    {
+                        reviewer: "Nirvan",
+                        rating: 5,
+                        comment: "Nice!!! :D",
+                    },
+                    {
+                        reviewer: "Lewis",
+                        rating: 4,
+                        comment: "Satisfactory."
+                    }
+                    {
+                        reviewer: "Tina",
+                        rating: 2,
+                        comment: "Came late but still works nicely."
+                    }
+                ]);
+            } else {
+                reject(`Failed to fetch reviews for product ID ${productId}`);
+            }
+        }, 1500);
     });
 };
